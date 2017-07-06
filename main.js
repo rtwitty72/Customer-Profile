a527d06c4031e906a50a8ec466218979d5608d77
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-2.local>
2017-07-06 17:01:39 -0400

    second commit

--- main.js
+++ main.js
@@ -2,12 +2,16 @@
 // 2. Loop over the data
 // 3. Display it in the `.customers` element
 
-const customers = document.getElementById('customers');
+const ul = document.getElementById('customers');
 const url = 'https://randomuser.me/api/?results=12';
 
 
 
 var customersList = document.getElementById('customers');
+
+function createNode(element) {
+      return document.createElement(element);
+  }
 
 
 fetch(url).then(function(response){
@@ -15,48 +19,67 @@ fetch(url).then(function(response){
       var customers = data.results;
       for (var i = 0; i < customers.length; i++){
       console.log(customers[i]);
-      var customerDiv = document.createElement('div');
+      var div = document.createElement('div');
+
+      var customersList = document.getElementById('customers');
+      var container = document.getElementById('container');
+      // var div = createNode('div');
+      container.append(customersList);
+      document.body.append(div);
 
 
-      
-      var customerPic = document.createElement('li');
+
+
+      var customerPic = document.createElement('img');
       var picture = customers[i].picture.large;
       console.log(picture);
-      var output = document.getElementById('customers', picture);
+      var img = createNode('img');
+      img.src = customers[i].picture.large;
+      document.body.appendChild(img);
 
-      var customerName = document.createElement('li');
+
+
+      var customerName = document.createElement('h3');
       var name = customers[i].name.first + " " + customers[i].name.last;
+      customerName.textContent = name;
+      customerName.className = 'name'
       console.log(name);
+      document.body.appendChild(customerName);
 
-      var customerEmail = document.createElement('li');
+      var customerEmail = document.createElement('h4');
       var email = customers[i].email;
+      customerEmail.textContent = email;
+      customerEmail.className = 'email';
       console.log(email);
+      document.body.appendChild(customerEmail);
 
-      var customerAddress = document.createElement('li');
+      var customerAddress = document.createElement('h4');
       var address = customers[i].location.street;
+      customerAddress.textContent = address;
+      customerAddress.className = 'address';
       console.log(address);
+      document.body.appendChild(customerAddress);
 
-      var customerAddress2 = document.createElement('li');
+      var customerAddress2 = document.createElement('h4');
       var address2 = customers[i].location.city + ", " + customers[i].location.state + customers[i].location.postcode;
+      customerAddress2.textContent = address2;
+      customerAddress2.className = 'address2';
       console.log(address2);
+      document.body.appendChild(customerAddress2);
 
-      var customerPhone = document.createElement('li');
+      var customerPhone = document.createElement('h4');
       var phone = customers[i].phone;
+      customerPhone.textContent = phone;
+      customerPhone.className = 'phone';
       console.log(phone);
-      customers.innerHTML += customers[i].phone;
-
-
-
-
-
-
-
-
-
-
+      document.body.appendChild(customerPhone);
 
 
 }
       }
+
+
+
+
   );
 });
